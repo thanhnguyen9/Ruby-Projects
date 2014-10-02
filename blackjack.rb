d1991c581f16de575747431df0693ae76e079075
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-10-01 20:24:27 -0500

    Finish actual random card for player and dealer

--- blackjack.rb
+++ blackjack.rb
@@ -19,185 +19,185 @@ class Cards
 
   def get_cards(player)
 
+    cards = []
+
+    cards << {
+      value: 1,
+      suit: "club"
+    }
+
+    cards << {
+      value: 1,
+      suit: "heart"
+    }
+
+    cards << {
+      value: 1,
+      suit: "diamond"
+    }
+
+    cards<< {
+      value: 1,
+      suit: "spade"
+    }
+
+    cards<< {
+      value: 2,
+      suit: "diamond"
+    }
+
+    cards<< {
+      value: 2,
+      suit: "club"
+    }
+
+    cards<< {
+      value: 2,
+      suit: "heart"
+    }
+
+    cards<< {
+      value: 2,
+      suit: "spade"
+    }
+
+    cards<< {
+      value: 3,
+      suit: "club"
+    }
+
+    cards<< {
+      value: 3,
+      suit: "diamond"
+    }
+
+    cards<< {
+      value: 3,
+      suit: "heart"
+    }
+
+    cards<< {
+      value: 3,
+      suit: "spade"
+    }
+    cards<< {
+      value: 4,
+      suit: "club"
+    }
+    cards<< {
+      value: 4,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 4,
+      suit: "heart"
+    }
+    cards<< {
+      value: 4,
+      suit: "spade"
+    }
+    cards<< {
+      value: 5,
+      suit: "heart"
+    }
+    cards<< {
+      value: 5,
+      suit: "club"
+    }
+    cards<< {
+      value: 5,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 5,
+      suit: "spade"
+    }
+    cards<< {
+      value: 6,
+      suit: "heart"
+    }
+    cards<< {
+      value: 6,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 6,
+      suit: "club"
+    }
+    cards<< {
+      value: 6,
+      suit: "spade"
+    }
+    cards<< {
+      value: 7,
+      suit: "heart"
+    }
+    cards<< {
+      value: 7,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 7,
+      suit: "club"
+    }
+    cards<< {
+      value: 7,
+      suit: "spade"
+    }
+    cards<< {
+      value: 8,
+      suit: "heart"
+    }
+    cards<< {
+      value: 8,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 8,
+      suit: "club"
+    }
+    cards<< {
+      value: 8,
+      suit: "spade"
+    }
+
+    cards<< {
+      value: 9,
+      suit: "heart"
+    }
+    cards<< {
+      value: 9,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 9,
+      suit: "club"
+    }
+    cards<< {
+      value: 9,
+      suit: "spade"
+    }
+    cards<< {
+      value: 10,
+      suit: "heart"
+    }
+    cards<< {
+      value: 10,
+      suit: "diamond"
+    }
+    cards<< {
+      value: 10,
+      suit: "club"
+    }
+    cards<< {
+      value: 10,
+      suit: "spade"
+    }
 
      @players.each do |player|
 
        if player.name == "Dealer"
 
-          cards = []
-
-          cards << {
-            value: 1,
-            suit: "club"
-          }
-
-          cards << {
-            value: 1,
-            suit: "heart"
-          }
-
-          cards << {
-            value: 1,
-            suit: "diamond"
-          }
-
-          cards<< {
-            value: 1,
-            suit: "spade"
-          }
-
-          cards<< {
-            value: 2,
-            suit: "diamond"
-          }
-
-          cards<< {
-            value: 2,
-            suit: "club"
-          }
-
-          cards<< {
-            value: 2,
-            suit: "heart"
-          }
-
-          cards<< {
-            value: 2,
-            suit: "spade"
-          }
-
-          cards<< {
-            value: 3,
-            suit: "club"
-          }
-
-          cards<< {
-            value: 3,
-            suit: "diamond"
-          }
-
-          cards<< {
-            value: 3,
-            suit: "heart"
-          }
-
-          cards<< {
-            value: 3,
-            suit: "spade"
-          }
-          cards<< {
-            value: 4,
-            suit: "club"
-          }
-          cards<< {
-            value: 4,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 4,
-            suit: "heart"
-          }
-          cards<< {
-            value: 4,
-            suit: "spade"
-          }
-          cards<< {
-            value: 5,
-            suit: "heart"
-          }
-          cards<< {
-            value: 5,
-            suit: "club"
-          }
-          cards<< {
-            value: 5,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 5,
-            suit: "spade"
-          }
-          cards<< {
-            value: 6,
-            suit: "heart"
-          }
-          cards<< {
-            value: 6,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 6,
-            suit: "club"
-          }
-          cards<< {
-            value: 6,
-            suit: "spade"
-          }
-          cards<< {
-            value: 7,
-            suit: "heart"
-          }
-          cards<< {
-            value: 7,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 7,
-            suit: "club"
-          }
-          cards<< {
-            value: 7,
-            suit: "spade"
-          }
-          cards<< {
-            value: 8,
-            suit: "heart"
-          }
-          cards<< {
-            value: 8,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 8,
-            suit: "club"
-          }
-          cards<< {
-            value: 8,
-            suit: "spade"
-          }
-
-          cards<< {
-            value: 9,
-            suit: "heart"
-          }
-          cards<< {
-            value: 9,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 9,
-            suit: "club"
-          }
-          cards<< {
-            value: 9,
-            suit: "spade"
-          }
-          cards<< {
-            value: 10,
-            suit: "heart"
-          }
-          cards<< {
-            value: 10,
-            suit: "diamond"
-          }
-          cards<< {
-            value: 10,
-            suit: "club"
-          }
-          cards<< {
-            value: 10,
-            suit: "spade"
-          }
           card1 = []
           card2 = []
 
@@ -248,57 +248,57 @@ class Cards
           p "#{player.name} is #{player.card_value}"
           @@compare << player.card_value
 
-        # else
-        #
-        #
-        #   card4 = []
-        #   card5 = []
-        #
-        #     card4 = cards.sample
-        #     card5 = cards.sample
-        #     @@cards << card4
-        #
-        #     while result.include? card5
-        #       card5 = card.sample
-        #     end
-        #     @@cards << card5
-        #
-        #
-        #   player.card_value = card4[:value] + card5[:value]
-        #   p "**********************************************"
-        #   p "#{player.name}'s first card is #{card4[:value]} #{card4[:suit]}"
-        #   p "#{player.name}'s second car is #{card5[:value]} #{card5[:suit]}"
-        #
-        #   if player.card_value > 21
-        #     loose()
-        #     break
-        #   end
-        #
-        #   p "Do you want to hit or stay, hit h for hit, s for stay"
-        #
-        #   user = gets.chomp
-        #
-        #   until user == "s"
-        #     card6 = cards.sample
-        #
-        #     while result.include? card6
-        #       card6 = cards.sample
-        #     end
-        #     @@cards << card6
-        #
-        #     player.card_value += card6[:value]
-        #
-        #     if player.card_value > 21
-        #       loose()
-        #       break
-        #     end
-        #
-        #     p "#{player.name}'s card is #{card6[:value]} #{card6[:suit]}, and your total is #{player.card_value}"
-        #     p "Do you want to hit or stay, hit h for hit, s for stay"
-        #     user = gets.chomp
-        #   end
-        #
-        #   p "Your total is #{player.card_value}"
+        else
+
+
+          card4 = []
+          card5 = []
+
+            card4 = cards.sample
+            card5 = cards.sample
+            @@cards << card4
+
+            while @@cards.include? card5
+              card5 = card.sample
+            end
+            @@cards << card5
+
+
+          player.card_value = card4[:value] + card5[:value]
+          p "**********************************************"
+          p "#{player.name}'s first card is #{card4[:value]} #{card4[:suit]}"
+          p "#{player.name}'s second car is #{card5[:value]} #{card5[:suit]}"
+
+          if player.card_value > 21
+            loose()
+            break
+          end
+
+          p "Do you want to hit or stay, hit h for hit, s for stay"
+
+          user = gets.chomp
+
+          until user == "s"
+            card6 = cards.sample
+
+            while @@cards.include? card6
+              card6 = cards.sample
+            end
+            @@cards << card6
+
+            player.card_value += card6[:value]
+
+            if player.card_value > 21
+              loose()
+              break
+            end
+
+            p "#{player.name}'s card is #{card6[:value]} #{card6[:suit]}, and your total is #{player.card_value}"
+            p "Do you want to hit or stay, hit h for hit, s for stay"
+            user = gets.chomp
+          end
+
+          p "Your total is #{player.card_value}"
 
         end
      end
@@ -362,4 +362,4 @@ card.add_player(dealer)
 #card.get_cards(thanh)
 card.get_cards(dealer)
 
-#card.who_win?
+card.who_win?
